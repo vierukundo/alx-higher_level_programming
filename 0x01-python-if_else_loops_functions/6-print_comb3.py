1c8240f1b5aca981aa8ed7fe98e5d872fed0482e
vierukundo <vierukundo@gmail.com>
2023-06-07 13:55:40 -0700

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(1,10):
+        if j > i:
+            if i == 8:
+                print('{}{}'.format(i, j))
+            print('{}{}'.format(i, j), end=", ")
