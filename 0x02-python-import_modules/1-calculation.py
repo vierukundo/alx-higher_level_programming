b445d27f6cf35be440982fe264a7b6f0f1928184
vierukundo <vierukundo@gmail.com>
2023-06-08 14:03:26 -0700

    program that imports functions from the file calculator_1.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import calculator_1
+
+
+def calc():
+    a = 10
+    b = 5
+    print('{} + {} = {}'.format(a, b, calculator_1.add(a, b)))
+    print('{} - {} = {}'.format(a, b, calculator_1.sub(a, b)))
+    print('{} * {} = {}'.format(a, b, calculator_1.mul(a, b)))
+    print('{} / {} = {}'.format(a, b, calculator_1.div(a, b)))
+
+
+if __name__ == "__main__":
+    calc()
