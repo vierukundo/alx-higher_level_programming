713d7031a17de592994c2d7ac07733890778ca61
vierukundo <vierukundo@gmail.com>
2023-06-09 02:50:28 -0700

    Build my own calculator!

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys, calculator_1
+    user_inputs = sys.argv
+    if len(user_inputs) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        exit(1)
+    if user_inputs[2] not in '+-*/':
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
+    a = int(user_inputs[1])
+    b = int(user_inputs[3])
+    if user_inputs[2] == '+':
+        result = calculator_1.add(a, b)
+    elif user_inputs[2] == '-':
+        result = calculator_1.sub(a, b)
+    elif user_inputs[2] == '*':
+        result = calculator_1.mul(a, b)
+    elif user_inputs[2] == '/':
+        result = calculator_1.div(a, b)
+    print('{} {} {} = {}'.format(a, user_inputs[2],  b, result))
