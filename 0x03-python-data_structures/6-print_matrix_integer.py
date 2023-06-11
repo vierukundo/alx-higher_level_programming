6666fdb3317969e7b4a8c8ee61dea99351704ea2
vierukundo <vierukundo@gmail.com>
2023-06-11 14:30:40 -0700

    prints matrix row per line

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    """Function that prints matrix rows per line"""
     if len(matrix) == 1 and len(matrix[0]) == 0:
         print()
     for row in matrix:
         for i in range(len(row)):
             if i < len(row) - 1:
-                print("{}".format(row[i]), end=" ")
+                print("{:d}".format(row[i]), end=" ")
             else:
-                print("{}".format(row[i]))
+                print("{:d}".format(row[i]))
