10e8ae242a0d91a3d5f685727e17eef04b28d87f
vierukundo <vierukundo@gmail.com>
2023-06-11 13:56:15 -0700

    modified function using reverse method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     """Function that prints a reversed list"""
-    index = len(my_list) - 1
-    while index >= 0:
-        print("{:d}".format(my_list[index]))
-        index -= 1
+    my_list.reverse()
+    for element in my_list:
+        print("{:d}".format(element))
