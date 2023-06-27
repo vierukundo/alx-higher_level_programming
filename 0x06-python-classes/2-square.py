553791f95c6a299213b0295696e6fc75db2fd82f
vierukundo <vierukundo@gmail.com>
2023-06-27 15:30:50 -0700

    python clases

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,23 +4,14 @@
 
 
 class Square:
-    """a class Square that defines a square by
-    Instantiation with optional size: def __init__(self, size=0):
-    size must be an integer, otherwise raise a TypeError exception
-    with the message size must be an integer
-    if size is less than 0, raise a ValueError exception
-    with the message size must be >= 0
-    """
+    """representation of square"""
     def __init__(self, size=0):
         """Initialization of instance of class
         args:
             size(int): size of square
         """
-        try:
-            if not isinstance(size, int):
-                raise TypeError("size must be an integer")
-            elif size < 0:
-                raise ValueError("size must be >= 0")
-        except(TypeError, ValueError):
-            raise
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
