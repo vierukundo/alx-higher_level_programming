5cc782d099ba583e4c33acc305c408811ca6117b
vierukundo <vierukundo@gmail.com>
2023-06-27 16:09:22 -0700

    square class with python getter and setter

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,6 +15,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """returns square area"""
         return self.__size ** 2
