b0649a07ed8852cff5a111860677a8ceff8f4824
vierukundo <vierukundo@gmail.com>
2023-06-27 16:23:29 -0700

    prints in stdout the square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# 5-square.py
+"""square class definition"""
+
+
+class Square:
+    """Representation of square"""
+    def __init__(self, size=0):
+        """initialization of class instance
+        args:
+            size(int): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """returns area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """prints square with character #"""
+        if self.__size == 0:
+            print()
+        else:
+            width = '#' * self.__size
+            for times in range(self.__size):
+                print(width)
