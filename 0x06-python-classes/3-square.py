7aa5a172462fb2b6fa25f80d4c32f41aa5d69b05
vierukundo <vierukundo@gmail.com>
2023-06-27 15:41:25 -0700

    return square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 3-square.py
+""" class defination called square"""
+
+
+class Square:
+    """representation of square"""
+    def __init__(self, size=0):
+        """Initialization of instance of class
+        args:
+            size(int): size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """returns square area"""
+        return self.__size ** 2
