13ceea8c729363ac8bfc1af9488ecf76a79e6c8a
vierukundo <vierukundo@gmail.com>
2023-06-27 14:40:24 -0700

    Python classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py
+class Square(object):
+    """class Square that defines a square by
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    """
+    def __init__(self, size):
+        """
+        This is --init-- documentation
+        args:
+            size: size of square and should be private
+        """
+        self.__size = size
