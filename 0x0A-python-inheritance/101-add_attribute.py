33c112ee378a8336a4f9e52cc1d4ea0146e3703b
vierukundo <vierukundo@gmail.com>
2023-07-13 17:25:20 -0700

    Python classs inheretence tasksk

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 # 101-add_attribute.py
-"""ffunction definition"""
+"""Defines a function that adds attributes to objects."""
 
 
-def add_attribute(obj, attr, value):
-    """Adds a new attribute to an object if it's possible."""
+def add_attribute(obj, att, value):
+    """Add a new attribute to an object if possible.
+
+    Args:
+        obj (any): The object
+        att (str): The name of the attribute
+        value (any): The value of attribute
+    Raises:
+        TypeError: If the attribute cannot be added.
+    """
     if not hasattr(obj, "__dict__"):
-        raise TypeError("Can't add new attribute")
-    else:
-        setattr(obj, attr, value)
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
