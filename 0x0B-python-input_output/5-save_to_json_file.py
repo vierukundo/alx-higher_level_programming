38ed913eed0181e2270da8ad2a4b547029786f0f
vierukundo <vierukundo@gmail.com>
2023-07-14 15:58:58 -0700

    Class to JSON

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         f.write(json.dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,5 +6,5 @@ import json
 
 def load_from_json_file(filename):
     """creates an Object from a JSON file"""
-    with open(filename, "r", encoding="utf-8") as f:
-        return json.loads(f.read())
+    with open(filename) as f:
+        return json.load(f)
