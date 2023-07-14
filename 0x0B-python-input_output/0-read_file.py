fbc0fb1295981654cc9bc08c404267f9a82520ac
vierukundo <vierukundo@gmail.com>
2023-07-14 13:13:29 -0700

    function that reads from file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="UTF8") as f:
-        print(f.read())
+        print(f.read(), end="")
