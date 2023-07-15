8951d48cbd14acfdd57cb6905668854bde9ef698
vierukundo <vierukundo@gmail.com>
2023-07-14 17:29:46 -0700

    python files operations tasks

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,6 +4,8 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file,
+    after each line containing a specific string"""
     with open(filename, 'r') as f:
         lines = f.readlines()
 
