4c04fa0130c8296e1e3a00820f6abd025eb0866a
vierukundo <vierukundo@gmail.com>
2023-07-15 15:51:43 -0700

    TDD

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,42 @@
 import unittest
-import tests.6-max_integer as 6-max_integer
+max_integer =  __import__("6-max_integer").max_integer
 
 class TestMax_integer(unittest.TestCase):
     def test_max_integer(self):
+        """random list"""
         t_list = [1, 4, 5, 6, 20, 34]
-        self.assertEqual(6-max_integer(t_list), 34)
+        self.assertEqual(max_integer(t_list), 34)
+
+    def test_empty_list(self):
+        """Test an empty list."""
+        empty = []
+        self.assertEqual(max_integer(empty), None)
+
+    def test_one_element_list(self):
+        """Test a list with a single element."""
+        one_element = [7]
+        self.assertEqual(max_integer(one_element), 7)
+
+    def test_floats(self):
+        """Test a list of floats."""
+        floats = [1.53, 6.33, -9.123, 15.2, 6.0]
+        self.assertEqual(max_integer(floats), 15.2)
+
+    def test_ints_and_floats(self):
+        """Test a list of ints and floats."""
+        ints_and_floats = [1.53, 15.5, -9, 15, 6]
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
+
+    def test_string(self):
+        """Test a string."""
+        string = "Brennan"
+        self.assertEqual(max_integer(string), 'r')
+
+    def test_list_of_strings(self):
+        """Test a list of strings."""
+        strings = ["Brennan", "is", "my", "name"]
+        self.assertEqual(max_integer(strings), "name")
+
+    def test_empty_string(self):
+        """Test an empty string."""
+        self.assertEqual(max_integer(""), None)
