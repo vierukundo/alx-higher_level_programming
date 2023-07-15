41f040aa0daf84b9319f1367ff605b5e5be4c9ca
vierukundo <vierukundo@gmail.com>
2023-07-15 16:08:34 -0700

    Test driven development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+# 3-say_my_name.py
+"""function definition"""
+
+
 def say_my_name(first_name, last_name=""):
     """
     Prints "My name is <first name> <last name>".
@@ -9,14 +14,12 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If first_name is not a string or last_name is not a string.
     """
-    if not isinstance(first_name, str):
+    if first_name is None or not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    full_name = first_name
-    if last_name:
-        full_name += " " + last_name
+    full_name = first_name + " " + last_name
 
     print("My name is", full_name)
