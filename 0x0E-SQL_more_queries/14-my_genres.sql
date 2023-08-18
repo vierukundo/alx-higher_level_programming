dde6095c76980019303075b95002f0c532c0cb50
vierukundo <vierukundo@gmail.com>
2023-08-18 12:12:11 -0700

    More MySQL queries

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,10 +4,7 @@ SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_show_genres.show_id =
-(
-	SELECT tv_shows.id
-	FROM tv_shows
-	WHERE title = "Dexter"
-)
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE title = "Dexter"
 ORDER BY tv_genres.name;
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,10 +4,7 @@ SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_show_genres.genre_id =
-(
-	SELECT tv_genres.id
-	FROM tv_genres
-	WHERE name = "Comedy"
-)
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE name = "Comedy"
 ORDER BY tv_shows.title;
