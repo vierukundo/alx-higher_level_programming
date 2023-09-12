bd827aca9c00e2edb4983763deb1de19ba4baf56
vierukundo <vierukundo@gmail.com>
2023-09-12 14:48:09 -0700

    function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let temp;
+  for (let i = 0; i < list.length / 2; i++) {
+    temp = list[i];
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = temp;
+  }
+  return (list);
+};
