90a26d8c49cb47f40a7710130d86ef97227077f7
vierukundo <vierukundo@gmail.com>
2023-09-11 18:51:55 -0700

    scripts that prints x times “C is fun” and square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ const side = [];
 
 if (isNaN(n)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (n > 0){
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
       side.push('X');
