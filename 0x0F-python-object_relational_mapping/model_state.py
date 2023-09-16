3d91706713d35ee9fabfdc4c7c00cdbc2a75122b
vierukundo <vierukundo@gmail.com>
2023-09-16 03:09:32 -0700

    Created State class using SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A Python script that defines a State class using SQLAlchemy.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(
+            Integer,
+            primary_key=True,
+            unique=True,
+            autoincrement=True,
+            nullable=False)
+    name = Column(String(128), nullable=False)
