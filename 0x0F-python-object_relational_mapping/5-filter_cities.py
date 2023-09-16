ada9683a893dc3d316a6ca1f95e8faa7fe89e897
vierukundo <vierukundo@gmail.com>
2023-09-16 03:34:33 -0700

    script that lists all cities of that state, using the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
             port=3306,
             user=username,
             passwd=password,
-            db=database_name,
-            charset="utf8")
+            db=database_name)
     cur = conn.cursor()
     query = "SELECT cities.name FROM cities JOIN states \
     ON cities.state_id = states.id \
@@ -24,8 +23,7 @@ if __name__ == "__main__":
     ORDER BY cities.id ASC"
     cur.execute(query, (sys.argv[4], ))
     cities = cur.fetchall()
-    if cities:
-        city_names = [city[0] for city in cities]
+    city_names = [city[0] for city in cities]
     print(", ".join(city_names))
     cur.close()
     conn.close()
