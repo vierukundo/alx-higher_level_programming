87c3e30b1cabde70ecf50823bf48762d07888ab0
vierukundo <vierukundo@gmail.com>
2023-09-16 01:26:35 -0700

    Improved task 2 so that it escapes SQL injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""a Python library that provides an interface
+for interacting with MySQL databases"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state = sys.argv[4]
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name,
+            charset="utf8")
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"
+    cur.execute(query.format(state))
+    states_rows = cur.fetchall()
+    for state in states_rows:
+        print(state)
+    cur.close()
+    conn.close()
