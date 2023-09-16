ada23970a02347bd62349bf0efffee11cd10afe7
vierukundo <vierukundo@gmail.com>
2023-09-16 02:26:00 -0700

    lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""a Python library that provides an interface
+for interacting with MySQL databases"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name,
+            charset="utf8")
+    cur = conn.cursor()
+    cur.execute(" \
+    SELECT cities.name \
+    FROM cities \
+    JOIN states \
+    ON cities.state_id = states.id \
+    WHERE states.name = %s \
+    ORDER BY cities.id ASC", (sys.argv[4], ))
+    cities = cur.fetchall()
+    if cities:
+        city_names = [city[0] for city in cities]
+    print(", ".join(city_names))
+    cur.close()
+    conn.close()
