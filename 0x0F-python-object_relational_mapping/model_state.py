105e1d365ccca5e928c1a3dea29a61e438c8acdb
vierukundo <vierukundo@gmail.com>
2023-09-16 12:23:49 -0700

    State class that inherits from Base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """A representation of state class that inherits from Base"""
     __tablename__ = 'states'
     id = Column(
             Integer,
