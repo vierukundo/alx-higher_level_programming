1221f96fcbb92da99b1d56b61dd52c406ae39a96
vierukundo <vierukundo@gmail.com>
2023-09-16 02:00:49 -0700

    script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""a Python library that provides an interface
+for interacting with MySQL databases"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name,
+            charset="utf8")
+    cur = conn.cursor()
+    cur.execute(" \
+    SELECT cities.id, cities.name, states.name \
+    FROM cities \
+    JOIN states \
+    ON cities.state_id = states.id \
+    ORDER BY cities.id ASC")
+    states_rows = cur.fetchall()
+    for state in states_rows:
+        print(state)
+    cur.close()
+    conn.close()
