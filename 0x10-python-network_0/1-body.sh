51bf117658d0449f46f9c369e3dea4ebcbfb4319
vierukundo <vierukundo@gmail.com>
2023-09-28 15:29:01 -0700

    Updated script to print only body response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the body of the response if status is 200
-if [ $(curl -sI "$1" | awk 'NR==1{print $2}') -eq 200 ]; then curl -s "$1"; fi
+if [ $(curl -sI "$1" | awk 'NR==1{print $2}') -eq 200 ]; then curl -sL "$1"; fi
