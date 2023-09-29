43f40119e568782a2561c58beb602145f3e5fad2
vierukundo <vierukundo@gmail.com>
2023-09-29 14:08:01 -0700

    sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Required modules"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    url = 'http://0.0.0.0:5000/search_user'
+    params = {'q': q}
+
+    try:
+        response = requests.post(url, data=params)
+        data = response.json()
+        if data:
+            user_id = data.get('id')
+            user_name = data.get('name')
+            print("[{}] {}".format(user_id, user_name))
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError:
+        print("Not a valid JSON")
